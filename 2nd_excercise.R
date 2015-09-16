780c52702f80d3a045670ac2c61982593d5fe634
ditacahyani <ditacahyani@ucla.edu>
2015-09-16 12:12:30 -0700

    last-morning

--- 2nd_excercise.R
+++ 2nd_excercise.R
@@ -34,4 +34,113 @@ size <- runif(dflength); size
 newdd <- cbind(dd, size); newdd
 head(dd)
 
+dd$mode =="MPF" # the result is true or false
+dd[which(dd$mode =="MPF"),] # hanya mengambil semua mode yg MPF di semua column
+
+dd[1:10, 2] # rows 1:10, column 2
+
+mpfs <- which(newdd$mode == 'MPF')
+mpf_swimmers <- newdd[mpfs,]
+head(mpf_swimmers)
+
+
+for(ii in 1:5){cat("\nte number is", ii)}
+# te number is 1
+# te number is 2
+# te number is 3
+# te number is 4
+# te number is 5
+
+for(ii in 1:5){cat("\nthe number is", ii)}
+# the number is 1
+# the number is 2
+# the number is 3
+# the number is 4
+# the number is 5
+
+notfish <- c("bat", "dolphin", "toad", "soldier")
+for(animal in notfish){cat(animal,"fish\n", sep="")}
+# batfish
+# dolphinfish
+# toadfish
+# soldierfish
+
+notfish <- c("bat", "dolphin", "toad", "soldier")
+for(animal in notfish){cat(animal,"\nfish", sep="")}
+# bat
+# fishdolphin
+# fishtoad
+# fishsoldier
+# fish
+
+for(animal in notfish){cat(animal,"fish","are tasty\n", sep="")}
+# batfishare tasty
+# dolphinfishare tasty
+# toadfishare tasty
+# soldierfishare tasty
+
+for(animal in notfish){cat(animal,"fish","are tasty\n")}
+# bat fish are tasty
+# dolphin fish are tasty
+# toad fish are tasty
+# soldier fish are tasty
+
+
+# while example
+xx <- 1
+xx <- 1
+while(xx < 5) {
+        xx <- xx+1; 
+        if (xx == 3) {
+                break; }
+}
+print(xx)
+
+# more complex examples
+xx <- 1
+while(xx < 10) {
+        cat("value of xx", xx, "\n")
+        xx <- xx+1
+        if (xx == 7) {
+                cat(" lucky number ", xx, "\n")}
+        else
+        {cat('not excited about the number', xx, "\n")
+        }
+}
+print(xx)
+
+
+xx <- 1
+while(xx < 10) {
+        cat("value of xx", xx, "\n")
+        xx <- xx+1
+        if (xx == 7) {
+                cat(" lucky number ", xx, "\n")}
+        else if (xx==2){
+                cat('not excited about the number', xx, "\n")}
+}
+
+
+xx <- 1
+while(xx < 10) {
+        # cat("value of xx", xx, "\n")
+        xx <- xx+1
+        if (xx == 7) {
+                cat(" lucky number ", xx, "\n")}
+        else if (xx==2){
+                cat('not excited about the number', xx, "\n")}
+}
+
+
+xx <- 1
+while(xx < 10) {
+        cat("value of xx", xx, "\n")
+
+        if (xx == 7) {
+                cat(" lucky number ", xx, "\n")}
+        else if (xx==2){
+                cat('not excited about the number', xx, "\n")}
+        xx <- xx+1
+}
+
 
