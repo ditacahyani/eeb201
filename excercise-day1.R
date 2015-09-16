8a38671a20aba8602af655dc1f91ceecb81a5eff
ditacahyani <ditacahyani@ucla.edu>
2015-09-16 13:40:34 -0700

    Day 1 excercise

---
+++ excercise-day1.R
@@ -0,0 +1,19 @@
+xx <- 1
+while(xx < 10) {
+        xx <- xx+1
+        if (xx == 10) {
+                cat(" * ", "\n")}
+        else
+        {cat(' ', "\n")
+        }
+}
+
+xx <- 1
+while(xx < 11) {
+        xx <- xx+1
+        if (xx == 10) {
+                cat(" * ", "\n")}
+        else
+        {cat(' * ', "\n")
+        }
+}
